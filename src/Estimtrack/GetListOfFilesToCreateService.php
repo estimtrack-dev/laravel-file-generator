550fbd19466d41852fcf6fc7040f1f0f8baa3fa8
Jimmy Balust <balust.jaume@gmail.com>
2020-11-18 12:04:22 +0100

    removed extra .php at end of filename

--- src/Estimtrack/GetListOfFilesToCreateService.php
+++ src/Estimtrack/GetListOfFilesToCreateService.php
@@ -10,25 +10,25 @@ class GetListOfFilesToCreateService
     public function execute($businessName, $entityName){
         $basePath = $businessName.'/'.$entityName.'/';
         return [
-            $basePath.'Domain/'.$entityName.'Entity.php',
-            $basePath.'Repositories/'.$entityName.'Repository.php',
+            $basePath.'Domain/'.$entityName.'Entity',
+            $basePath.'Repositories/'.$entityName.'Repository',
 
-            $basePath.'Services/Get'.$entityName.'Service.php',
-            $basePath.'Services/Create'.$entityName.'Service.php',
-            $basePath.'Services/Update'.$entityName.'Service.php',
+            $basePath.'Services/Get'.$entityName.'Service',
+            $basePath.'Services/Create'.$entityName.'Service',
+            $basePath.'Services/Update'.$entityName.'Service',
             $basePath.'Services/Delete'.$entityName.'Service.php',
 
-            $basePath.'UseCases/Get'.$entityName.'/Get'.$entityName.'Request.php',
-            $basePath.'UseCases/Get'.$entityName.'/Get'.$entityName.'UseCase.php',
+            $basePath.'UseCases/Get'.$entityName.'/Get'.$entityName.'Request',
+            $basePath.'UseCases/Get'.$entityName.'/Get'.$entityName.'UseCase',
 
-            $basePath.'UseCases/Create'.$entityName.'/Create'.$entityName.'Request.php',
-            $basePath.'UseCases/Create'.$entityName.'/Create'.$entityName.'UseCase.php',
+            $basePath.'UseCases/Create'.$entityName.'/Create'.$entityName.'Request',
+            $basePath.'UseCases/Create'.$entityName.'/Create'.$entityName.'UseCase',
 
-            $basePath.'UseCases/Update'.$entityName.'/Update'.$entityName.'Request.php',
-            $basePath.'UseCases/Update'.$entityName.'/Update'.$entityName.'UseCase.php',
+            $basePath.'UseCases/Update'.$entityName.'/Update'.$entityName.'Request',
+            $basePath.'UseCases/Update'.$entityName.'/Update'.$entityName.'UseCase',
 
-            $basePath.'UseCases/Delete'.$entityName.'/Delete'.$entityName.'Request.php',
-            $basePath.'UseCases/Delete'.$entityName.'/Delete'.$entityName.'UseCase.php',
+            $basePath.'UseCases/Delete'.$entityName.'/Delete'.$entityName.'Request',
+            $basePath.'UseCases/Delete'.$entityName.'/Delete'.$entityName.'UseCase',
 
         ];
     }
